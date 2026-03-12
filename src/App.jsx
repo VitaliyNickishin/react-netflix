5ac5855af3a1a5f54b38328a6e951f323e3adff6
Vitaly Nickishin <nickishin34@gmail.com>
2026-03-12 17:49:59 +0200

    connected tailwind, created component MovieCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { MovieCard } from "./components/MovieCard";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="min-h-screen w-full bg-black text-white px-6 py-5">
+      <header className="mb-10 flex items-center">
+        <img src="/logo.svg" alt="Netflix" className="h-8 w-auto" />
+      </header>
+      <main className="flex gap-6">
+        <MovieCard image="/1.webp" rating={8.8} />
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
