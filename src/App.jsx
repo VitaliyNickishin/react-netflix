bf7d3ad00cfd57150a7dbe705447b3d37a0d2b4e
Vitaly Nickishin <nickishin34@gmail.com>
2026-03-16 18:35:17 +0200

    createContext for swith theme dark/light

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Home } from "./pages/Home";
 
 function App() {
   return (
-    <div className="min-h-screen w-full bg-black text-white px-6 py-5">
+    <div className="min-h-screen w-full bg-white dark:bg-black text-black dark:text-white px-6 py-5">
       <Home />
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import { useDebounce } from "../hooks/useDebounce";
 import { MoviesList } from "../components/MoviesList";
 import { SearchInput } from "../components/SearchInput";
 import { Spinner } from "../components/Spinner";
+import { useTheme } from "../hooks/useTheme";
 
 export const Home = () => {
   const { movies, isLoading, error } = useMovies();
@@ -19,15 +20,35 @@ export const Home = () => {
     );
   }, [movies, debouncedSearch]);
 
+  const { theme, toggleTheme } = useTheme();
+
   return (
     <>
       <header className="mb-10 flex items-center justify-between">
         <img src="/logo.svg" alt="Netflix" className="h-8" />
 
-        <SearchInput
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
+        <div className="flex gap-1">
+          <SearchInput
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <div
+            className="cursor-pointer sm:w-26 text-sm px-1 sm:px-3 py-1 rounded border text-white bg-black dark:bg-transparent border-black/20 dark:border-white/10 hover:bg-white hover:text-black transition"
+            onClick={toggleTheme}
+          >
+            {theme === "dark" ? (
+              <>
+                <span>☀️</span>
+                <span className="hidden sm:inline"> Light</span>
+              </>
+            ) : (
+              <>
+                <span>🌙</span>
+                <span className="hidden sm:inline"> Dark</span>
+              </>
+            )}
+          </div>
+        </div>
       </header>
       <main>
         {isLoading && <Spinner />}
